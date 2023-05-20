3496ad937d6b4163900cb02181ec0cba7c6ef27b
Jack Lingle <captainjack0818@gmail.com>
2023-05-20 17:21:12 -0400

    Add file structure

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new ESLintPlugin()],
+  devServer: {
+    static: './dist/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+    resolve: {
+      extensions: ['*', '.js'],
+    },
+  },
+};
