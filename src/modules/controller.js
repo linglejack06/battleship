9cfb712ee0db75262cfcac5b3534598e16ea3b82
Jack Lingle <captainjack0818@gmail.com>
2023-05-24 11:56:14 -0400

    add position values to random generator

--- src/modules/controller.js
+++ src/modules/controller.js
@@ -4,11 +4,31 @@ import Player from './player';
 import Ship from './ship';
 import Doc from './doc';
 
-const battleships = ['Destroyer', 'Submarine', 'Cruiser', 'Battleship', 'Carrier'];
+let battleships = ['Destroyer', 'Submarine', 'Cruiser', 'Battleship', 'Carrier'];
 const human = new Player('Jack');
 const board1 = new Board(human, []);
 const board2 = new Board(null, []);
 
+function setComputerBoard() {
+  battleships = ['Destroyer', 'Submarine', 'Cruiser', 'Battleship', 'Carrier'];
+  for (let i = 0; i < 5; i += 1) {
+    let randomCoords = [Math.floor(Math.random() * 9), Math.floor(Math.random() * 9)];
+    const position = Math.floor(Math.random() * 2);
+    let orientation = '';
+    if (position === 0) {
+      orientation = 'horizontal';
+    } else {
+      orientation = 'vertical';
+    }
+    let ship = new Ship(battleships[battleships.length - 1], orientation, randomCoords);
+    while (!board2.checkShipPosition(ship)) {
+      randomCoords = [Math.floor(Math.random() * 9), Math.floor(Math.random() * 9)];
+      ship = new Ship(battleships[battleships.length - 1], orientation, randomCoords);
+    }
+    board2.placeShip(ship);
+    battleships.pop();
+  }
+}
 function setPlayerBoard() {
   const tags = Doc.setupGame(board1, board2, battleships[battleships.length - 1]);
   let count = 0;
@@ -16,28 +36,21 @@ function setPlayerBoard() {
     e.preventDefault();
     const coords = tags.coords.value.split(', ').map((coord) => parseInt(coord, 10));
     const ship = new Ship(battleships[battleships.length - 1], 'horizontal', coords);
-    board1.placeShip(ship);
-    battleships.pop();
-    tags.coords.value = '';
-    count += 1;
+    if (board1.checkShipPosition(ship)) {
+      board1.placeShip(ship);
+      Doc.populateBoard(board1);
+      battleships.pop();
+      tags.coords.value = '';
+      count += 1;
+    } else {
+      tags.coords.value = '';
+      tags.instructions.textContent = `Error: ${ship.type} must have atleast ${ship.length} consecutive spaces and not be placed on another ship.`;
+    }
     if (count === 5) {
+      setComputerBoard();
       Doc.renderGame(board1, board2);
     }
   });
-}
-// eslint-disable-next-line no-unused-vars
-function setComputerBoard() {
-  if (battleships.length === 0) return 'done';
-  Doc.setupGame(board1, board2, battleships[battleships.length - 1]);
-  const randomCoords = [Math.floor(Math.random() * 9), Math.floor(Math.random() * 9)];
-  const ship = new Ship(battleships[battleships.length - 1], 'horizontal', randomCoords);
-  if (board2.checkShipPosition(ship)) {
-    board2.placeShip(ship);
-    battleships.pop();
-    setComputerBoard();
-  } else {
-    setComputerBoard();
-  }
 }
 export default function startGame() {
   setPlayerBoard();
--- src/modules/doc.js
+++ src/modules/doc.js
@@ -7,7 +7,7 @@ export default (function Doc() {
   const box2 = document.createElement('div');
   box2.classList.add('game-board');
   document.body.appendChild(box2);
-  function _populateBoard(box, board) {
+  function populateBoard(board, box = box1) {
     for (let i = 0; i < board.length; i += 1) {
       const col = board[i];
       const gameColumn = document.createElement('div');
@@ -32,17 +32,12 @@ export default (function Doc() {
     form.appendChild(coordInput);
     return coordInput;
   }
-  function _renderStartForm(box, player, nextShip, error) {
+  function _renderStartForm(box, player, nextShip) {
     const formContainer = document.createElement('div');
     formContainer.classList.add('game-text', 'form-container');
     const instructions = document.createElement('h2');
-    if (error) {
-      instructions.textContent = error;
-      instructions.classList.add('error');
-    } else {
-      instructions.classList.add('instructions');
-      instructions.textContent = `Place your ${nextShip}`;
-    }
+    instructions.classList.add('instructions');
+    instructions.textContent = `Place your ${nextShip}`;
     formContainer.appendChild(instructions);
     const form = document.createElement('form');
     form.classList.add(player.name, 'coord-form');
@@ -61,15 +56,15 @@ export default (function Doc() {
     box1.innerHTML = '';
     box2.innerHTML = '';
     const formObj = _renderStartForm(box1, board1.player, nextShip, error);
-    _populateBoard(box1, board1.arr);
-    _populateBoard(box2, board2.arr);
+    populateBoard(board1.arr, box1);
+    populateBoard(board2.arr, box2);
     return formObj;
   }
   function renderGame(board1, board2) {
     box1.innerHTML = '';
     box2.innerHTML = '';
-    _populateBoard(box1, board1.arr);
-    _populateBoard(box2, board2.arr);
+    populateBoard(board1.arr, box1);
+    populateBoard(board2.arr, box2);
   }
-  return { setupGame, renderGame };
+  return { setupGame, renderGame, populateBoard };
 }());
